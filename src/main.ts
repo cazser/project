9868482b92590e58f3b3572c7211afe274084785
cazser <2264712072@qq.com>
2021-07-14 21:30:40 +0800

    使用ant design

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import './index.css'
 import { createWebHashHistory , createRouter} from 'vue-router'
 import Frank from './components/Frank.vue'
 import Frank2 from './components/Frank2.vue'
+import Antd from 'ant-design-vue';
+
+import 'ant-design-vue/dist/antd.css';
 
 const history = createWebHashHistory()
 const router  = createRouter({
@@ -17,4 +20,7 @@ const router  = createRouter({
 
 const app = createApp(App)
 app.use(router)
+app.config.productionTip = false;
+
+app.use(Antd);
 app.mount('#app')
